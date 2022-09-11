d0986d13f43e7f470e8e598f561e6ff5dd0b0ab5
Cavernos <louisdescavernes@gmail.com>
2022-09-11 13:13:54 +0200

    Restructuration du projet

--- front/src/react_component/layouts/Tutos/TutoTopics.jsx
+++
@@ -1,9 +0,0 @@
-export function TutoTopics(){
-    return <div className="topics">
-        <div className="topics-create-info">
-            <a href="forum/base-de-donnee-1" className="topics-name active">Problème de Base de donnée</a>
-            <p>Crée par Cavernos</p>
-            <p>Commencé le 23 Février</p>
-        </div>
-    </div>
-}
--- front/tests/react/Api.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect , it} from "vitest";
-import { getName } from "../../src/class/Api";
-
-describe('Test de la classe API', () =>{
-    it('Test de la fonction get name'), () => {
-        expect(getName(5)).toBe('Et quam laborum voluptates qui molestiae.')
-    }
-
-})
