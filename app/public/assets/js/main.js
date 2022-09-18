a4b7952ab79f93ff1f29549a49a1064e4af846ca
Cavernos <louisdescavernes@gmail.com>
2022-09-18 23:04:32 +0200

    Ajout d'un carousel pour les actualite au HOMEé

--- app/public/assets/js/main.js
+++ app/public/assets/js/main.js
@@ -1,6 +1,5 @@
-
-
-timeago().register('fr_FR', fr)
+import Carousel from './carousel.js'
+/**timeago().register('fr_FR', fr)
 
 timeago().render(document.querySelectorAll('.timeago'), 'fr_FR')
 
@@ -10,4 +9,12 @@ flatpickr('.datepicker', {
   altFormat: 'j F Y, H:i',
   dateFormat: 'Y-m-d H:i:s',
   locale: French
+})*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  new Carousel(document.querySelector("#carousel"), {
+      slidesToScroll: 10,
+      slidesVisible: 10,
+      pagination: true
+  })
 })
